92093ba46959604a727226649d00e8e1aa0fa6d7
Emma Mendez <emmamendez07@gmail.com>
2021-11-08 11:12:29 +0000

    c# grade book inital study

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,7 +10,12 @@ namespace GradeBook
             var numbers = new double [3];
             numbers[0] = 12.7;
             numbers[1] = 10.3;
-            
+            numbers[2] = 6.11;
+
+            var result = numbers[0];
+            result = result + numbers[1];
+            result = result + numbers[2];
+            Console.WriteLine(result);
 
            if (args.Length > 0)
            {
