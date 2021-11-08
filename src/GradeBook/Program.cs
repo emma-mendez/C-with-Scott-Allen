29339a84af3664f6c59083dc9122dba0896dcab8
Emma Mendez <emmamendez07@gmail.com>
2021-11-08 11:07:08 +0000

    c# grade book inital study

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = new double [3];
+            numbers[0] = 12.7;
+            numbers[1] = 10.3;
+            
+
+           if (args.Length > 0)
+           {
+            Console.WriteLine($"Hello, {args[0]}!");
+           }
+           else {
+               Console.WriteLine("Hello Emma Your ON Baby");
+           }
+        }
+    }
+}
